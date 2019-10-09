e2a7828fe0133bae10a357645a919e4741f837cd
soyidwahyud <soyidwahyu4@gmail.com>
2019-10-09 15:16:39 +0700

    Bottom Navigation

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/Activities/Activities/MainActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/Activities/Activities/MainActivity.java
@@ -1,16 +1,62 @@
 package polinema.ac.id.androiduistarter.fragments.Activities.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import polinema.ac.id.androiduistarter.R;
+import polinema.ac.id.androiduistarter.fragments.Activities.Fragments.DiscountFragment;
+import polinema.ac.id.androiduistarter.fragments.Activities.Fragments.foodFragment;
+import polinema.ac.id.androiduistarter.fragments.Activities.Fragments.movieFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new foodFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(MainActivity.this);
+    }
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new foodFragment();
+                break;
+            case R.id.action_movie:
+                fragment = new movieFragment();
+                break;
+            case R.id.action_discount:
+                fragment = new DiscountFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
